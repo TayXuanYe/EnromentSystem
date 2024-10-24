1a24a6f1f95d23a6a76da97acc6da3f2b375e7f7
TayXuanYe <pikaqiu04082004@gmail.com>
2024-10-24 22:42:23 +0800

    POP UP WINDOWS SUCCESSFUL

---
+++ EnromentSystem/Scripts/LoginPagePopUpWindows.js
@@ -0,0 +1,13 @@
+﻿// Get popups, close buttons and trigger links
+var modal = document.getElementById("popUpWindows");
+var openModalLink = document.getElementById("openModalLink");
+
+openModalLink.onclick = function (event) {
+    modal.style.display = "block";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
